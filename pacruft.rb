2d94c09a1cf39272e22bc13f3acf4a78f790e307
patrick brisbin <pbrisbin@gmail.com>
2011-03-30 13:36:07 -0400

    restructure; cleanup

--- pacruft.rb
+++ pacruft.rb
@@ -11,53 +11,49 @@ class Package
   attr_reader :name, :accessed
 
   def initialize(name)
-    recent    = nil
+    @name     = name
     @accessed = -1
 
-    get_files(name).each { |fname|
+    max = nil
+    owned_files.each { |fname|
       begin
-        atime  = File.atime(fname)
-        recent = atime if !recent or recent < atime
+        atime = File.atime(fname)
+        max   = atime if !max or atime > max
       rescue
         # can't read; skip
       end
     }
 
-    @name     = name
-    @accessed = Time.now - recent if recent
+    @accessed = Time.now - max if max
+  end
+
+  def owned_files 
+    return `pacman -Qql #{ @name }`.split("\n").find_all { |fname| fname =~ /.*[^\/]$/ }
   end
 
   def is_old?
-    return true if @accessed > $threshold
+    return true if @accessed > $T
     return false
   end
 end
 
-# get all explicitly installed packages
-def get_packages 
-  `pacman -Qqe`.split("\n").each { |p| yield Package.new(p) }
-end
-
-# get all normal files owned by a package
-def get_files(pkgname) 
-  return `pacman -Qql #{ pkgname }`.split("\n").find_all { |fname| fname =~ /.*[^\/]$/ }
-end
-
-$threshold = (60 * 60 * 24 * 180)
+$T = (60 * 60 * 24 * 180)
 
 OptionParser.new do |o|
-  o.on('-3',  'set threshold as 3 months') { $threshold = (60 * 60 * 24 * 90)  }
-  o.on('-6',  'set threshold as 6 months') { $threshold = (60 * 60 * 24 * 180) }
-  o.on('-12', 'set threshold as 1 year'  ) { $threshold = (60 * 60 * 24 * 365) }
+  o.on('-3',  'set threshold as 3 months') { $T = (60 * 60 * 24 * 90)  }
+  o.on('-6',  'set threshold as 6 months') { $T = (60 * 60 * 24 * 180) }
+  o.on('-12', 'set threshold as 1 year'  ) { $T = (60 * 60 * 24 * 365) }
 
   o.on('-h', '--help', 'display this') { puts o; exit }
 
   begin o.parse!
   rescue OptionParser::InvalidOption => e
-    puts e
-    puts o
+    puts e; puts o;
     exit 1
   end
 end
 
-get_packages { |pkg| puts pkg.name if pkg.is_old? }
+`pacman -Qqe`.split("\n").each { |p|
+  pkg = Package.new(p)
+  puts pkg.name if pkg.is_old?
+}
