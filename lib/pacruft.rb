037c61233a4a4c443c7c7d6631f247f64683ca6e
patrick brisbin <pbrisbin@gmail.com>
2011-10-23 14:48:44 -0400

    refactor to gem-ish layout

--- lib/pacruft.rb
+++ lib/pacruft.rb
@@ -1,10 +1,3 @@
-#!/usr/bin/ruby
-#
-# pbrisbin 2011
-#
-###
-require 'optparse'
-
 class Fixnum # add some time helpers to Fixnum
   def seconds; return self               end
   def minutes; return self         * 60  end
@@ -45,27 +38,4 @@ class Package
     # assumption: a non-directory will end in a non-slash
     return `pacman -Qql #{ @name }`.split("\n").find_all { |fname| fname =~ /.*[^\/]$/ }
   end
-end
-
-t = 0
-
-OptionParser.new do |o|
-  o.on('-y', '--years  <years>' ) { |y| t += y.to_i.years  }
-  o.on('-m', '--months <months>') { |m| t += m.to_i.months }
-  o.on('-d', '--days   <days>'  ) { |d| t += d.to_i.days   }
-  o.on('-h', '--help'           ) { puts o; exit }
-
-  begin o.parse!
-  rescue OptionParser::InvalidOption => e
-    puts e; puts o;
-    exit 1
-  end
-end
-
-# no options passed?
-t = 6.months if t == 0
-
-`pacman -Qqe`.split("\n").each do |p|
-  pkg = Package.new(p)
-  puts pkg.name if pkg.is_older_than? t
 end
